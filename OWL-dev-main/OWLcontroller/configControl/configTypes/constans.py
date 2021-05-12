d7ece038cf0264f639c011043535f716e1de7771
Elad <elad1593210@gmail.com>
2021-05-12 17:22:47 +0300

    Fixing Todos

---
+++ OWL-dev-main/OWLcontroller/configControl/configTypes/constans.py
@@ -0,0 +1,7 @@
+class const():
+    # Legacy mode
+    # ROOT_FOLDER = r'..\\' # when running from confParser
+    LEGACY_ROOT_FOLDER = ""  # when running from controlPc
+    LEGACY_MODE_CONF_SUFFIX = 'ini'
+    DEFAULT_CONF_FILE_PATH = LEGACY_ROOT_FOLDER + 'defaultConfiguration.json'
+    SEQUANCE_FILE = 'sequancefile'
