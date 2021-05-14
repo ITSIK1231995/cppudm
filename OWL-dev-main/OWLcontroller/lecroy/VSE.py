2c738f5e6f637b44f68adcf3ff4fe0f2b5b4a0e4
Elad <elad1593210@gmail.com>
2021-05-14 16:24:30 +0300

    fixing that the it failed after twice because the list of created trace

--- OWL-dev-main/OWLcontroller/lecroy/VSE.py
+++ OWL-dev-main/OWLcontroller/lecroy/VSE.py
@@ -1,5 +1,6 @@
 import os
 from win32com.client import DispatchWithEvents, WithEvents
+from copy import deepcopy
 
 class PEEvent(object):
     def OnTraceCreated(self, trace):
@@ -8,58 +9,44 @@ class PEEvent(object):
             self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,"\nPEEvent::OnTraceCreated - %s" % trace)
         except Exception as e:
             print("PEEvent::OnTraceCreated failed with exception: %s" % e)
-            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,
-                                                                   "\nPEEvent::OnTraceCreated failed with exception: %s" % e)
+            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,"\nPEEvent::OnTraceCreated failed with exception: %s" % e)
+
     def OnStatusReport(self, subsystem, state, percent_done):
         try:
-            print("PEEvent::OnStatusReport - subsystem:{0}, state:{1}, progress:{2}".format(subsystem, state,
-                                                                                            percent_done))
-            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,
-                                                                   "PEEvent::OnStatusReport - subsystem:{0}, state:{1}, progress:{2}".format(subsystem, state,
-                                                                                            percent_done))
+            print("PEEvent::OnStatusReport - subsystem:{0}, state:{1}, progress:{2}".format(subsystem, state,percent_done))
+            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog, "PEEvent::OnStatusReport - subsystem:{0}, state:{1}, progress:{2}".format(subsystem, state,percent_done))
         except Exception as e:
             print("PEEvent::OnStatusReport failed with exception: %s" % e)
-            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,
-                                                                   "PEEvent::OnStatusReport failed with exception: %s" % e)
+            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,"PEEvent::OnStatusReport failed with exception: %s" % e)
+
 # class for for handling VSE events
 class VSEventHandler(object):
     def OnVScriptReportUpdated(self, new_line, tag):
         try:
             print("VSEventHandler::OnVScriptReportUpdated - %s tag:%s" % (new_line, tag))
-            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,
-                                                                   "VSEventHandler::OnVScriptReportUpdated - %s tag:%s" % (new_line, tag))
+            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,"VSEventHandler::OnVScriptReportUpdated - %s tag:%s" % (new_line, tag))
         except Exception as e:
             print("VSEventHandler::OnVScriptReportUpdated - failed with exception: %s" % e)
-            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,
-                                                                   "VSEventHandler::OnVScriptReportUpdated - failed with exception: %s" % e)
+            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,"VSEventHandler::OnVScriptReportUpdated - failed with exception: %s" % e)
+
     def OnVScriptFinished(self, script_name, result, tag):
         try:
-            print("VSEventHandler::OnVScriptFinished - {script_name}, {result}, {tag}".format(script_name=script_name,
-                                                                                              result=result,
-                                                                                              tag=tag))
-            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,
-                                                                   "VSEventHandler::OnVScriptFinished - {script_name}, {result}, {tag}".format(script_name=script_name,
-                                                                                              result=result,
-                                                                                              tag=tag))
+            print("VSEventHandler::OnVScriptFinished - {script_name}, {result}, {tag}".format(script_name=script_name,result=result,tag=tag))
+            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,"VSEventHandler::OnVScriptFinished - {script_name}, {result}, {tag}".format(script_name=script_name,result=result,tag=tag))
         except Exception as e:
             print("VSEventHandler::OnVScriptFinished - failed with exception: %s" % e)
-            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,
-                                                                   "VSEventHandler::OnVScriptFinished - failed with exception: %s" % e)
+            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,"VSEventHandler::OnVScriptFinished - failed with exception: %s" % e)
 
     def OnNotifyClient(self, eventId, eventBody, tag):
         try:
             print("VSEventHandler::OnNotifyClient - eventId: %s, eventBody: %s, tag: %s" % (eventId, eventBody, tag))
-            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,
-                                                                   "VSEventHandler::OnNotifyClient - eventId: %s, eventBody: %s, tag: %s" % (eventId, eventBody, tag))
+            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,"VSEventHandler::OnNotifyClient - eventId: %s, eventBody: %s, tag: %s" % (eventId, eventBody, tag))
         except Exception as e:
             print("VSEventHandler::OnNotifyClient - failed with exception: %s" % e)
-            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,
-                                                                   "VSEventHandler::OnNotifyClient - failed with exception: %s" % e)
+            self.__class__.controller.updateRunTimeStateInTerminal(self.__class__.hostPc, self.__class__.testLog,"VSEventHandler::OnNotifyClient - failed with exception: %s" % e)
 
 class VSE():
     def startVerificationScriptEngine(self, traceFullPathAndName, vScriptFullPathAndName,hostPc, testLog, controller):
-        from copy import deepcopy
-        #self.CopyOfPEEvent = type('copyOfPEevent', PEEvent.__bases__, dict(PEEvent.__dict__))
         self.CopyOfPEEvent = deepcopy(PEEvent)
         self.CopyOfPEEvent.hostPc = hostPc
         self.CopyOfPEEvent.testLog = testLog
@@ -76,13 +63,10 @@ class VSE():
         Result = VSEngine.RunVScript()  # run VSE script and get result
         if Result == 1:
             print ("VSE's results: PASSED !!! ")
-            controller.updateRunTimeStateInTerminal(hostPc, testLog,
-                                                     "\nVSE's results: PASSED !!! ")
+            controller.updateRunTimeStateInTerminal(hostPc, testLog,"\nVSE's results: PASSED !!! ")
         else:
             print ("VSE's results: FAILED !!!")
-            controller.updateRunTimeStateInTerminal(hostPc, testLog,
-                                                    "\nVSE's results: FAILED !!!")
+            controller.updateRunTimeStateInTerminal(hostPc, testLog,"\nVSE's results: FAILED !!!")
         os.system("TASKKILL /F /IM PETracer.exe")
         print("VSE script execution has finished")
-        controller.updateRunTimeStateInTerminal(hostPc, testLog,
-                                                "\nVSE script execution has finished")
+        controller.updateRunTimeStateInTerminal(hostPc, testLog,"\nVSE script execution has finished")
